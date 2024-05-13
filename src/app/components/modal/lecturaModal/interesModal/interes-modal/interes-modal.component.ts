f0e9f32b0cf5458b44161bf620c67ed1e35848ea
María Escribano Verde <maria.escribanoverde@gmail.com>
2024-05-13 12:06:20 +0200

    Fix

--- src/app/components/modal/lecturaModal/interesModal/interes-modal/interes-modal.component.ts
+++ src/app/components/modal/lecturaModal/interesModal/interes-modal/interes-modal.component.ts
@@ -1,11 +1,14 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
-import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { ActivatedRoute } from '@angular/router';
 import { NotificationService } from 'src/app/services/notification/notification.service';
 import { FormErrorStateMatcher } from 'src/app/shared/errors/form-error-state-matcher';
 import { InputErrorStateMatcherExample } from 'src/app/shared/errors/input-error-state-matcher';
 import { Book } from 'src/app/shared/models/book/book';
+import { FechasModalComponent } from '../../fechasModal/fechas-modal/fechas-modal.component';
+import { ModalInfo, ModalInfoPages } from 'src/app/shared/models/modal/modal';
+import { UserValoracionModalComponent } from '../../userValoracionModal/user-valoracion-modal/user-valoracion-modal.component';
 
 @Component({
   selector: 'app-interes-modal',
@@ -15,6 +18,10 @@ import { Book } from 'src/app/shared/models/book/book';
 export class InteresModalComponent implements OnInit {
 
   modalInfo?: Book;
+
+  modalInfoFechas?: ModalInfoPages;
+  modalInfoValoracion?: ModalInfo;
+
   matcher!: FormErrorStateMatcher;
   formularioLibro!: FormGroup;
 
@@ -24,7 +31,7 @@ export class InteresModalComponent implements OnInit {
     private route: ActivatedRoute,
     private formBuilder: FormBuilder,
     private formControl: InputErrorStateMatcherExample,
-    private notification: NotificationService,
+    private dialog: MatDialog
   ) {
     if (data && data.modalInfo) {
       this.modalInfo = data.modalInfo;
@@ -45,9 +52,41 @@ export class InteresModalComponent implements OnInit {
   }
 
   submit() {
+    const estadoMarcado = this.formularioLibro.get('estado')?.value;
+    if (this.modalInfo) {
+      this.modalInfoFechas = {
+        id: this.modalInfo.id,
+        title: this.modalInfo.titulo,
+        pages: this.modalInfo.paginasTotales,
+      };
+      this.modalInfoValoracion = {
+        id: this.modalInfo.id,
+        title: this.modalInfo.titulo,
+      };
+      if (this.modalInfoFechas || this.modalInfoValoracion) {
+        if (estadoMarcado === 'PROCESO') {
+          const dialogFechas = this.dialog.open(FechasModalComponent, {
+            width: '50%',
+            data: {
+              modalInfo: this.modalInfoFechas
+            }
+          });
+        }
+        if (estadoMarcado === 'LEÍDO') {
+          const dialogValoracion = this.dialog.open(UserValoracionModalComponent, {
+            width: '50%',
+            data: {
+              modalInfo: this.modalInfoValoracion
+            }
+          });
+        }
+      }
+    }
+
     //TODO LLAMADA EDIT ESTADO LECTURA
-    console.log('GUARDAR');
-  }
+    this.dialogRef.close();
+    console.log(estadoMarcado);
+  } 
 
   specificError(modelAttribute: string, errorAttribute: string) {
     return this.matcher.isErrorStateSpecific(
