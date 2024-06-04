b64907206fdfa559fbbe4b4edfe0982092605062
María Escribano Verde <maria.escribanoverde@gmail.com>
2024-06-04 08:05:01 +0200

    Fix componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GeneroTipoService } from './services/genero/genero-tipo.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  constructor(private generoTipoService: GeneroTipoService) { }
+  
+  ngOnInit(): void {
+    this.generoTipoService.getGeneroTipo().subscribe();
+  }
 }
